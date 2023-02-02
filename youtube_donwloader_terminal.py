0ca6633d2d5c56f8b69d35f8cf4c8f4d7eca477e
Dominik Förster <dominikforster@MacBook-Air-von-Dominik.local>
2023-02-02 22:39:50 +0100

    Added mp3 Support

--- youtube_donwloader_terminal.py
+++ youtube_donwloader_terminal.py
@@ -1,23 +1,62 @@
 from pytube import YouTube
+import os
+from pydub import AudioSegment
 
-path= "/Users/dominikforster/Downloads"
+path= ""
 
 print("Youtube URl:")
 url= input()
-
 yt = YouTube(url)
+format=""
 
-formats = yt.streams.filter(progressive=True, file_extension='mp4')
-video = max(formats, key=lambda x: x.resolution)
+while (format!="mp3") and (format!="mp4"):
+    print("Choose Format(mp4/mp3): ")
+    format = input()
 
-yt_video_info = (
-    "Title: "+yt.title+"\n"+
-    "Channel: "+yt.author+"\n"+
-    "Length: "+yt.length+"\n"
-)
+def DownloadVideoAsMp4():
+    formats = yt.streams.filter(progressive=True, file_extension='mp4')
+    video = max(formats, key=lambda x: x.resolution)
+    print("Downloading "+yt.title+" as MP4")
+    video.download(path)
+    print("Finished!")
 
-print(yt_video_info)
-print("downloading...")
-video.download(path)
-print("Finished!")
+def DownloadVideoAsMp3(path=path): 
+     formats = yt.streams.filter(progressive=True, file_extension='mp4')
+     video = max(formats, key=lambda x: x.resolution)
+     print("Downloading "+yt.title+"...")
+     video.download(path)
+     delete_file = video.default_filename
+     mp3_file = video.default_filename.replace('.mp4', '.mp3')
+     audio = AudioSegment.from_file(video.default_filename, format='mp4')
+     print("Convert "+delete_file+" to mp3...")
+     audio.export(mp3_file, format='mp3')
+     os.remove(delete_file)
+     print("Finished!")
+
+
+def VideoInfo():
+    yt_video_info = (
+        "Title: "+yt.title+"\n"+
+        "Channel: "+yt.author+"\n"+
+        "Release: "+str(yt.publish_date)+"\n"   
+        "Views: "+str(yt.views)+"\n"+
+        "Format: "+format
+    )
+    print(yt_video_info)
+
+VideoInfo()
+print('Confirm Download ? (y/n)')
+answer = input()
+
+if answer == "y" and "Y":
+    if format == "mp3":
+        DownloadVideoAsMp3()
+    if format == "mp4":
+        DownloadVideoAsMp4()
+else:
+    print('Canceled!')
+    exit
+
+
+
 
